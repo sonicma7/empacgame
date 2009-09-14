197c6029d714c0463531ff01394eecd7d08755c8
hotsho111@gmail.com <hotsho111@gmail.com@16c3c540-a0b3-11de-b0cc-ab0ad15e60d0>
2009-09-14 19:45:55 +0000

    Framework code

---
+++ game.py
@@ -0,0 +1,34 @@
+import pygame
+import config
+import splashscreen
+import layermanager
+import sys
+
+from pygame.locals import *
+
+pygame.font.init()
+pygame.init()
+
+screen = pygame.display.set_mode(config.screensize)#, pygame.FULLSCREEN)
+pygame.display.set_caption(config.title)
+masterclock = pygame.time.Clock()
+
+mainmenu = splashscreen.SplashScreen(screen)
+manager = layermanager.LayerManager()
+manager.layerStack.append(mainmenu)
+
+while True:
+    masterclock.tick(60)
+    pygame.event.pump()
+    keys = pygame.key.get_pressed()  #Check if keys have been pressed
+    if keys[K_ESCAPE]:
+        sys.exit()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()  
+        manager.HandleEvent(event)
+
+    manager.Update()
+    manager.Render(screen)
+    pygame.display.flip()
