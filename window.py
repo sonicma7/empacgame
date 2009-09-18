c9939d545417c7d92c087d04ad77935275d59207
hotsho111@gmail.com <hotsho111@gmail.com@16c3c540-a0b3-11de-b0cc-ab0ad15e60d0>
2009-09-18 00:29:41 +0000

    Finished background and finished player movement and glass attacking

--- window.py
+++ window.py
@@ -1,19 +1,22 @@
 import pygame
 
 class Window():
-    def __init__(self,position):
+    def __init__(self,position,health):
         self.window = pygame.image.load("art/window.png")
         self.windowRect = self.window.get_rect()
         self.brokenWindow = pygame.image.load("art/brokenwindow.png")
+        self.altwindow  = pygame.image.load("art/player.png")
         self.brokenWindowRect = self.brokenWindow.get_rect()
-        self.health = 3
+        self.health = health
         self.position = position
 
     def HandleEvent(self, event):
         pass
 
-    def Update(self):
+    def Update(self):        
         if self.health > 0:
+            self.drawWindow = self.window
+        elif self.health == -1:
             self.drawWindow = self.window
         else:
             self.drawWindow = self.brokenWindow
